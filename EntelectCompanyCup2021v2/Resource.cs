3ee03dc9a9e5fdc1b7517d229b6c10818ebc1aca
Donald Dunlop <donald.dunlop@entelect.co.za>
2021-07-22 16:50:38 +0200

    feat: Added class for Resource

---
+++ EntelectCompanyCup2021v2/Resource.cs
@@ -0,0 +1,22 @@
+﻿namespace EntelectCompanyCup2021v2
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PointsPerRawUnit { get; set; }
+        public int PointsPerProcessedUnit { get; set; }
+        public int ProcessingTime { get; set; }
+        public decimal PointsPerTimeProcessed { get; set; }
+
+        public Resource(int id, string name, int pointsPerRawUnit, int pointsPerProcessedUnit, int processingTime, decimal pointsPerTimeProcessed)
+        {
+            Id = id;
+            Name = name;
+            PointsPerRawUnit = pointsPerRawUnit;
+            PointsPerProcessedUnit = pointsPerProcessedUnit;
+            ProcessingTime = processingTime;
+            PointsPerTimeProcessed = pointsPerTimeProcessed;
+        }
+    }
+}
