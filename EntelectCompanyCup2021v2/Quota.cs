53755e735fdae7629a57adebc890422d3dcd74e6
jarodsmk <jarodsmk@gmail.com>
2021-07-22 17:03:09 +0200

    added quota and resource cluster classes

---
+++ EntelectCompanyCup2021v2/Quota.cs
@@ -0,0 +1,20 @@
+﻿namespace EntelectCompanyCup2021v2
+{
+    public class Quota
+    {
+        public int ResourceId { get; }
+        public int QuotaAmount { get; }
+
+        public Quota(int resourceId, int quotaAmount)
+        {
+            ResourceId = resourceId;
+            QuotaAmount = quotaAmount;
+        }
+
+        public static Quota Create(string line)
+        {
+            var data = line.Split('|');
+            return new Quota(int.Parse(data[0]), int.Parse(data[1]));
+        }
+    }
+}
